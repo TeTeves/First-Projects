9b59e72f3742d8c76c7e459f80a01ba52b7ad2ac
TeTeves <85762759+TeTeves@users.noreply.github.com>
2021-06-11 16:15:49 -0300

    Código básico em Java
    Calculadora de IMC criado com Java.

---
+++ CalculadoraIMC.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CalculadoraIMC {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		//variaveis
+		
+		float peso, altura;
+		float imc;
+		
+		//entrada de dados
+		
+		do {
+			System.out.println("Digite sua altura");
+			altura = leia.nextFloat();
+		if (altura > 2.50 || altura <= 0) {
+			System.err.println("A altura não pode ser igual ou inferior a 0 e nem superior à 2,50m");	
+		}
+		}while (altura > 2.50 || altura <= 0);
+		
+		do {
+			System.out.println("Digite o seu peso");
+			peso = leia.nextFloat();
+		if (peso <=0) {
+			System.err.println("o peso deve ser maior que 0");
+		}
+		}while(peso <= 0);
+		imc = peso / (altura * altura);
+		
+		// resultados
+		System.out.println(" O seu IMC é: " +imc);
+	}
+
+}
